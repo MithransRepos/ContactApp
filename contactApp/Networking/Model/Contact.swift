ff9d8797ed71e8d65fe61906a33d4cc11f4849f5
MithransRepos <mithran@radiusagent.com>
2019-05-23 09:30:50 +0530

    added Contact model & network manager functions

---
+++ contactApp/Networking/Model/Contact.swift
@@ -0,0 +1,25 @@
+//
+//  Contact.swift
+//  contactApp
+//
+//  Created by Mithran Natarajan on 5/23/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Contact
+struct Contact: Codable {
+    let id: Int
+    let firstName, lastName, profilePic: String
+    let favorite: Bool
+    let url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case profilePic = "profile_pic"
+        case favorite, url
+    }
+}
