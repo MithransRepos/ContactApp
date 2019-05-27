c2d952e2273bc6563534175436363cb6ad59e39b
MithransRepos <mithran@radiusagent.com>
2019-05-27 18:55:11 +0530

    add/edit wip

--- contactApp/Views/ContactDetailHeader.swift
+++ contactApp/Views/ContactDetailHeader.swift
@@ -16,29 +16,9 @@ class ContactDetailHeader: UITableViewHeaderFooterView {
     @IBOutlet var favoriteView: UIView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var favoriteImageView: UIImageView!
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        initView()
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        initView()
-//    }
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//    }
-//
-//    private func initView() {
-//        Bundle.main.loadNibNamed(String(describing: ContactDetailHeader.self), owner: self, options: nil)
-//        contentView.frame = bounds
-//        addSubview(contentView)
-//        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-//    }
 
     func setupView(contact: Contact) {
+        favoriteImageView.setRounded()
         avatarImageView.setImage(from: contact.profilePicUrl)
         avatarImageView.setBorder(withColor: .white, borderWidth: 2)
         nameLabel.text = contact.firstName
